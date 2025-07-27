cb41bd9d501b859a851a90c7420cd15d1afd72ff
ACIE <alpha562023@gmail.com>
2025-07-27 19:33:19 +0300

    ui.shdln intergartion, taiwind  styling

--- src/components/GroupList.tsx
+++ src/components/GroupList.tsx
@@ -1,30 +1,14 @@
-import { MouseEvent } from "react";
+import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
+
 function GroupList() {
-  let items = ["acie", "ammy", "amiko", "optomes", "jones"];
-  const movies = ["John Wick", "Alpines", "Young wolf", "Prime Bees"];
-
-  const handleClick = (Event: MouseEvent) => {
-    alert("button clicked!");
-  };
-
-  if (items.length === 0) {
-    return (
-      <>
-        <h1>This is a group list</h1>
-        <p>No items found</p>
-      </>
-    );
-  }
   return (
     <>
-      <h1> This is a group List</h1>
-      <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
-            {item}
-          </li>
-        ))}
-      </ul>
+      <Alert>
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components and dependencies to your app using the cli.
+        </AlertDescription>
+      </Alert>
     </>
   );
 }
